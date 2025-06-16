de6a5f4b6898e62c68f934473088b15d69bb7383
wojciechHnatiuk <wojciech.hnatiuk@gmail.com>
2025-06-16 19:28:34 +0200

    refactor: move chunkSize calculation inside the loop for clarity

--- cypress/support/functions/customArrayMethods.ts
+++ cypress/support/functions/customArrayMethods.ts
@@ -1,12 +1,12 @@
 export function splitIntoParts<T>(arr: T[], parts: number): T[][] {
-	const result: T[][] = []
-	const chunkSize = Math.ceil(arr.length / parts)
+  const result: T[][] = []
 
-	for (let i = 0; i < parts; i++) {
-		const start = i * chunkSize
-		const end = start + chunkSize
-		result.push(arr.slice(start, end))
-	}
+  for (let i = 0; i < parts; i++) {
+    const chunkSize = Math.ceil(arr.length / parts)
+    const start = i * chunkSize
+    const end = start + chunkSize
+    result.push(arr.slice(start, end))
+  }
 
-	return result
+  return result
 }
